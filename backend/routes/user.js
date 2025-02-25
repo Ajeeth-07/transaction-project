dfafdb2e71841541acaca7aa5e22dab80032d626
Ajeeth Shinde <89684651+Ajeeth-07@users.noreply.github.com>
2025-02-25 19:22:43 +0530

    signinroute added, bcrypt

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
-const User = require("../db");
+const {User} = require("../db");
 const { JWT_SECRET } = require("../config");
-const app = express();
+const bcrypt = require("bcrypt");
 
-app.post("/signup", async(req, res) => {
-    const username = req.body.username;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const password = req.body.lastName;
+router.post("/signup", async(req, res) => {
+    try{
+        const {username, firstName, lastName, password} = req.body;
 
     if(!(username && password && firstName)){
         return res.status(403).json({msg : "required field names missing"});
@@ -19,15 +17,53 @@ app.post("/signup", async(req, res) => {
 
     if(existingUser) return res.status(400).json({msg : "username is already taken"});
 
+    const hashedPass = await bcrypt.hash(password, 10);
+
     await User.create({
         username,
-        password
+        firstName,
+        lastName,
+        password : hashedPass
     });
 
-    const token = jwt.sign({username}, JWT_SECRET);
+    const token = jwt.sign({username, id:User._id}, JWT_SECRET);
 
     res.status(200).json({msg : "User created successfully", token})
+    }catch(error){
+        console.error("Error during signup:", error);
+        res.status(500).json({msg : "Internal Server Error"});
+    }
+    
 
+})
+
+router.post("/signin", async(req, res) => {
+    try{
+        const {username, password} = req.body;
+
+    if(!(username && password)){
+        res.status(500).json({msg : "username and password required"});
+    }
+
+    //finding username
+    const user = await User.findOne({username});
+    if(!user){
+        return res.status(400).json({msg : "Invalid credentials"});
+    }
+
+    //comparing pass
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(400).json({msg:"Invalid credentials"});
+
+
+    const token = jwt.sign({id:user._id, username}, JWT_SECRET);
+
+    res.status(200).json({msg : "Signin Successfull", token});
+    }catch(err){
+        console.error("Error signing in "+ err);
+        res.status(404).json({msg: "Internal Server error"});
+    }
+    
 })
 
 
