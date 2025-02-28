1705553e77af78285a2fa03e24ad694b9425fe5e
Ajeeth Shinde <89684651+Ajeeth-07@users.noreply.github.com>
2025-02-28 17:57:55 +0530

    some more improvents, done with project

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,4 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 export const Appbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/signin");
+  };
+
   return (
     <div className="shadow h-14 flex justify-between items-center px-4">
       <div>Transaction Project</div>
@@ -6,6 +15,30 @@ export const Appbar = () => {
         <div className="mr-4">Hello</div>
         <div className="rounded-full h-12 w-12 bg-slate-200 flex items-center justify-center text-xl">
           U
+        </div>
+        <div className="items-center pt-1 ml-2">
+          <button
+            type="button"
+            className="text-gray-900 bg-white hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800 flex-row-reverse"
+            onClick={handleLogout}
+          >
+            <svg
+              className="w-5 h-5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+            Logout
+          </button>
         </div>
       </div>
     </div>
--- frontend/src/files/Send.jsx
+++ frontend/src/files/Send.jsx
@@ -1,12 +1,46 @@
 import { useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
+
 export const Send = () => {
     const [searchParams] = useSearchParams();
     const id = searchParams.get("id");
     const name = searchParams.get("name");
     const [amount, setAmount] = useState("");
     const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const handleTransfer = async() => {
+        try{
+            setLoading(true)
+            const res = await axios.post(
+          "http://localhost:3000/api/v1/account/transfer",
+          {
+            to: id,
+            amount,
+          },
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        );
+        if (res.status === 200) {
+            console.log(res.data);
+          setMessage("Transfer Successfull");
+            navigate("/dashboard")
+        }else if(res.status === 400){
+            setMessage("Insufficient Balance")
+        }
+        }catch(err){
+            console.error("Error: " + err);
+            setMessage("Server Down, please try later")
+        }finally{
+            setLoading(false);
+        }
+        
+    }
+
   return (
     <div className="flex justify-center h-screen bg-gray-100">
       <div className="h-full flex flex-col justify-center">
@@ -39,22 +73,10 @@ export const Send = () => {
                   placeholder="Enter amount"
                 />
               </div>
-              <button onClick={async() => {
-                const res = await axios.post("http://localhost:3000/api/v1/account/transfer", {
-                    to : id,
-                    amount,
-                }, {
-                    headers : {
-                        Authorization : "Bearer " + localStorage.getItem("token")
-                    }
-                })
-                if(res.status === 200){
-                    setMessage("Transfer Successfull")
-                }
-              }} className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
-                Initiate Transfer
+              <button onClick={handleTransfer} disabled={loading} className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
+                {loading ? "Processing..." : "Initiate Transfer"}
               </button>
-              {message}
+              {message && <div className="text-center mt-2">{message}</div>}
             </div>
           </div>
         </div>
